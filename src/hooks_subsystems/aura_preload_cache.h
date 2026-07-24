c5d656ca1a4c230b43c5b6b9be395b6dd35b0a84
Aleksander <pupresupre@gmail.com>
2026-07-24 22:33:46 +0300

    feat: Asynchronous aura data prefetcher

--- src/hooks_subsystems/aura_preload_cache.h
+++ src/hooks_subsystems/aura_preload_cache.h
@@ -6,4 +6,7 @@ namespace AuraPreloadCache {
     bool Init();
     void Shutdown();
     void PreloadAuraTexture(const std::string& path);
+
+    // Feature 40 (0x008DA550): Asynchronous Aura Data Prefetcher
+    void OptimizeSub8DA550_AsyncAura(int spellId, float x, float y, float z);
 }
