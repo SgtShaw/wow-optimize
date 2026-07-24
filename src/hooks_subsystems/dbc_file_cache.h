0bf96ab8b633b285b3267731682590b84b2cf8ca
Aleksander <pupresupre@gmail.com>
2026-07-24 22:33:48 +0300

    feat: Asynchronous DBC file preloader

--- src/hooks_subsystems/dbc_file_cache.h
+++ src/hooks_subsystems/dbc_file_cache.h
@@ -6,4 +6,7 @@ namespace DbcFileCache {
     bool Init();
     void Shutdown();
     void* LookupRecord(void* dbc, uint32_t id);
+
+    // Feature 47 (0x006337D0): Asynchronous DBC Preloader
+    void OptimizeSub6337D0_AsyncDBC(const char* dbcFileName);
 }
