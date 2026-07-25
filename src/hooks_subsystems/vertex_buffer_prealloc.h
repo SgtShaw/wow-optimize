9b6d68e389ff78849727d9f7806733f1b2dacea3
Aleksander <pupresupre@gmail.com>
2026-07-25 03:01:47 +0300

    feat: Lock-free memory pool slot allocators and atomic slot updating fastpaths

--- src/hooks_subsystems/vertex_buffer_prealloc.h
+++ src/hooks_subsystems/vertex_buffer_prealloc.h
@@ -6,4 +6,9 @@ namespace VertexBufferPrealloc {
     void Shutdown();
     void* AllocateBuffer(size_t size);
     void FreeBuffer(void* ptr);
+
+    // Memory Pool & Lock-Free Slot Optimizations
+    void* OptimizeSub4DAB40_PoolSlotAlloc(size_t size);
+    void* OptimizeSub4C74F0_ObjPoolAlloc(size_t size);
+    bool OptimizeSub927A40_LockFreeUpdate(volatile LONG* slot, const void* src, void* dst, size_t size);
 }
